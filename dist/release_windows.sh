7ad8de54762cbd69408dc5beb1a358579bc533c5
370025263 <370025263@qq.com>
2023-08-11 19:12:12 +0800

    [bug]修复了打包脚本不能加密的问题

--- dist/release_windows.sh
+++ dist/release_windows.sh
@@ -1,14 +1,18 @@
 # 自动打包当前文件到release/windows_ver
 
-## 加密项目代码到code
-pyarmor g -O code/ -r -i ../../npc_engine
-
 ## 删除之前的code
 rm -r ./code/*
+
+## 加密项目代码到code
+pyarmor g -O code/npc_engine/  -r  ../../npc_engine/src
+mv code/npc_engine/pyarmor_runtime_* code/npc_engine/src
+
+
+## 删除之前的code
 rm -r ./release/windows_ver/code/*
 
 ## copy数据文件到code
-rsync -av --exclude-from='release_ignore.conf' ../../npc_engine code/
+rsync -av --ignore-existing  --exclude-from='release_ignore.conf' ../../npc_engine code/
 
 ## code 移动到release
 cp -r code/npc_engine release/windows_ver/code
