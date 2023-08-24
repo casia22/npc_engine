304fc67fa7eda4fb9d0cac1c7df69ad99c54aaae
yangzejun0312 <yzj_cs_ilstar@163.com>
2023-08-24 12:34:47 +0800

    [debug]: modify import

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,1 +1,1 @@
-#from npc_engine.src.config.config import *
+from npc_engine.src.config.config import *
--- src/npc/__init__.py
+++ src/npc/__init__.py
@@ -1,2 +1,2 @@
-#from npc_engine.src.npc.npc import *
-#from npc_engine.src.npc.conversation import *
+from npc_engine.src.npc.npc import *
+from npc_engine.src.npc.conversation import *
