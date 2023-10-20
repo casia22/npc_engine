e7f5a8e7a3079aac6d3ab13cadbde0fff8b395d8
370025263 <370025263@qq.com>
2023-10-20 14:13:26 +0800

    [fix]修复了npc的action空间不会随着自身所在场景更新，而是集成全局最大action的问题

--- src/engine.py
+++ src/engine.py
@@ -260,7 +260,10 @@ class NPCEngine:
         scenario_name: str = json_data["scenario_name"]
         topic: str = json_data["topic"]
         length: str = json_data["length"]
-        stream: bool = json_data["stream"]
+        try:
+            stream: bool = json_data["stream"]
+        except:
+            stream: bool = False
         memory_k = json_data["memory_k"]
 
         # 初始化群体描述、心情和记忆
@@ -636,9 +639,6 @@ class NPCEngine:
         else:
             action_log: str = action_item.get_log(npc_name, json_data["object"], json_data["parameters"],
                                                   reason=json_data["reason"])
-        # 更新NPC允许的动作
-        npc.set_known_actions(list(self.action_dict.keys()))
-        npc.set_action_dict(self.action_dict)
         # 更新NPC的状态
         npc.set_state(json_data['npc_state'])
         # 更新NPC的场景属性(自动更新scenario_knowledge和自动更新scenario属性)
@@ -696,8 +696,6 @@ class NPCEngine:
         # 获得NPC的引用
         npc_name = json_data["npc_name"]
         npc = self.npc_dict[npc_name]
-        # 更新NPC允许的动作
-        npc.set_known_actions(list(self.action_dict.keys()))
         # 更新NPC的状态
         npc.set_state(json_data['npc_state'])
         npc.set_scenario(scenario=json_data["scenario_name"])
@@ -772,8 +770,6 @@ class NPCEngine:
         # 获得NPC的引用
         npc_name = json_data["npc_name"]
         npc = self.npc_dict[npc_name]
-        # 更新NPC允许的动作
-        npc.set_known_actions(list(self.action_dict.keys()))
         # 更新NPC的状态
         npc.set_state(json_data['npc_state'])
         npc.set_scenario(scenario=json_data["scenario_name"])
